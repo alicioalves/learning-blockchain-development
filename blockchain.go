5fe81046d7fc7b6d69b129552a5e505922958f2d
Alicio <alicio.alvesm@gmail.com>
2022-07-12 20:49:53 -0300

    added method to mine blocks

--- blockchain.go
+++ blockchain.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -27,4 +28,11 @@ func (b Block) calculateHash() string {
 	blockData := b.previousHash + string(data) + b.timestamp.String() + strconv.Itoa(b.pow)
 	blockHash := sha256.Sum256([]byte(blockData))
 	return fmt.Sprintf("%x", blockHash)
+}
+
+func (b *Block) mine(difficulty int) {
+	for !strings.HasPrefix(b.hash, strings.Repeat("0", difficulty)) {
+		b.pow++
+		b.hash = b.calculateHash()
+	}
 }
