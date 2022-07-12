651bbd1ce3cb7e4a31a17c4dcf359d9f1f03421c
Alicio <alicio.alvesm@gmail.com>
2022-07-12 20:41:44 -0300

    added block struct

---
+++ blockchain.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"time"
+)
+
+type Block struct {
+	data         map[string]interface{}
+	hash         string
+	previousHash string
+	timestamp    time.Time
+	pow          int
+}
