99923881df59ce5964e12fc1dc205fdd7dafbeae
Alicio <alicio.alvesm@gmail.com>
2022-07-12 20:47:28 -0300

    added method to calculate hash of a block

--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -16,4 +20,11 @@ type Blockchain struct {
 	genesisBlock Block
 	chain        []Block
 	difficulty   int
+}
+
+func (b Block) calculateHash() string {
+	data, _ := json.Marshal(b.data)
+	blockData := b.previousHash + string(data) + b.timestamp.String() + strconv.Itoa(b.pow)
+	blockHash := sha256.Sum256([]byte(blockData))
+	return fmt.Sprintf("%x", blockHash)
 }
